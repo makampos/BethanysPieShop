f05a0876897cba9d388433ff527e5130a920fa09
makampos <matheus.campos@edu.unifil.br>
2020-08-15 16:09:49 -0300

    update

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -23,5 +23,16 @@ namespace BethanysPieShop.Controllers
             piesListViewModel.CurrentCategory = "Cheese Cakes";
             return View(piesListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+            {
+                return NotFound();
+            }
+
+            return View(pie);
+        }
     }
 }
