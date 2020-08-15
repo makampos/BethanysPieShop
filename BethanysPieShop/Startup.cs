efcd64da0a33ef2c1c0e837bcc307042beeef5c1
makampos <matheus.campos@edu.unifil.br>
2020-08-14 23:37:40 -0300

    update

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,16 +10,31 @@ namespace BethanysPieShop
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IPieRepository, MockPieRepository>();
-            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+            
+             services.AddDbContext<AppDbContext>(options =>
+                 options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+
+            // services.AddDbContext<AppDbContext>(options =>
+            //     options.UseMySql(Configuration.GetConnectionString("DefaultConnection"),
+            //         x => x.MigrationsAssembly("BethanysPieShop")));
+
+            services.AddScoped<IPieRepository, PieRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
             //services.AddTransient()
             //services.AddSingleton()
             services.AddControllersWithViews();
         }
+
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
