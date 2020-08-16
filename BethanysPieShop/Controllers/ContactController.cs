306d539168a204a15969b65177de832f3dedf9f4
makampos <matheus.campos@edu.unifil.br>
2020-08-16 15:06:49 -0300

    update

---
+++ BethanysPieShop/Controllers/ContactController.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BethanysPieShop/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BethanysPieShop.TagHelpers
+{
+    public class EmailTagHelper :TagHelper
+    {
+        public string Adress { get; set; }
+        public string Content { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href","mailto:" + Adress);
+            output.Content.SetContent(Content);
+        }
+    }
+}
