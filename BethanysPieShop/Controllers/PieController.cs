a379bf5b2c6ff03a166f2e016a8483baf0f078c0
makampos <matheus.campos@edu.unifil.br>
2020-08-16 14:26:24 -0300

    update

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using BethanysPieShop.Models;
 using BethanysPieShop.ViewModel;
 using Microsoft.AspNetCore.Mvc;
@@ -14,15 +16,42 @@ namespace BethanysPieShop.Controllers
             _pieRepository = pieRepository;
             _categoryRepository = categoryRepository;
         }
+        
+        // Change for the viewcomponent
+        // public ViewResult List()
+        // {
+        //     PiesListViewModel piesListViewModel = new PiesListViewModel();
+        //     piesListViewModel.Pies = _pieRepository.AllPies;
+        //     
+        //     piesListViewModel.CurrentCategory = "Cheese Cakes";
+        //     return View(piesListViewModel);
+        // }
 
-        public ViewResult List()
+        public ViewResult List(string category)
         {
-            PiesListViewModel piesListViewModel = new PiesListViewModel();
-            piesListViewModel.Pies = _pieRepository.AllPies;
-            
-            piesListViewModel.CurrentCategory = "Cheese Cakes";
-            return View(piesListViewModel);
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory =
+                    _categoryRepository.AllCategories.FirstOrDefault(c =>
+                        c.CategoryName  == category)?.CategoryName;
+            }
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
+
 
         public IActionResult Details(int id)
         {
